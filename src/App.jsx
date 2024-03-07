02c392d2e9bea487b5f7fbfcce77deee178a7347
vbasoon <vbasoon@gmail.com>
2024-03-07 12:21:08 +0200

    pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import {Route, BrowserRouter as Router, Routes} from 'react-router-dom';
-import Navbar from './components/Navbar'
+
+import {Home, About, Projects, Contacts} from './pages';
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
@@ -7,10 +9,10 @@ const App = () => {
       <Router>
         <Navbar/>
         <Routes>
-          <Route path="/" element={'Home'} />
-          <Route path="/about" element={'About'} />
-          <Route path="/projects" element={'Projects'} />
-          <Route path="/contacts" element={'Contact Us'} />
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contacts" element={<Contacts />} />
         </Routes>
       </Router>
     </main>
