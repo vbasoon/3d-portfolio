9a4177edb4ac1f61aeb89c00cd2195d234ba2adb
vbasoon <vbasoon@gmail.com>
2024-03-07 11:41:34 +0200

    nav links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,10 +6,10 @@ const Navbar = () => {
       <NavLink to="/" className="w-10 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md">
         <p>Logo</p>
         </NavLink>
-        {/* <nav className="flex text-lg gap-7 font-medium">
+        <nav className="flex text-lg gap-7 font-medium">
           <NavLink to="/about">About</NavLink>
 
-        </nav> */}
+        </nav>
     
     </header>
   )
