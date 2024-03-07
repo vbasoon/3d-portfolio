74d4d29e6a1cd5b9f4234b297aa32f69fcd4e156
vbasoon <vbasoon@gmail.com>
2024-03-07 11:04:55 +0200

    react-router-dom install fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
+import {Route, BrowserRouter as Router, Routes} from 'react-router-dom'
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </div>
+    <main className="bg-slate-300/20">
+      <Router>
+        Navbar
+        <Routes>
+          <Route path="/" element={'Home'} />
+        </Routes>
+      </Router>
+    </main>
   )
 }
 
