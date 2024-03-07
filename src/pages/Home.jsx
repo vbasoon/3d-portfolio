9a131b3ad4e4b08b0a00f7b75bbfd918ea71eba7
vbasoon <vbasoon@gmail.com>
2024-03-07 12:26:16 +0200

    Popup

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,9 @@
 const Home = () => {
   return (
     <section className="w-full h-screen relative">
-      
+      <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
+        POPUP
+      </div>
     </section>
   )
 }
