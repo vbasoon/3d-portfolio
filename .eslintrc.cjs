80fe9523313ee29c75d8731deea7c6412b401d97
vbasoon <vbasoon@gmail.com>
2024-03-08 19:10:25 +0200

    adjust Island

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,12 +7,12 @@ module.exports = {
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'src'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
-    'react/jsx-no-target-blank': 'off',
+    
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,6 +12,7 @@ const Home = () => {
   const adjustIslandForScreenSize = () => {
     let screenScale = null; 
     let screenPosition = [0, -6.5, -43];
+    let rotation = [0.1, 4.7, 0];
 
     if(window.innerWidth < 768) {
       screenScale = [0.9, 0.9, 0.9];
@@ -19,10 +20,10 @@ const Home = () => {
       screenScale = [1, 1, 1];
     }
 
-    return [screenScale, screenPosition]
+    return [screenScale, screenPosition, rotation]
   }
 
-  const [islandScale, islandPosition] = adjustIslandForScreenSize();
+  const [islandScale, islandPosition, islandRotation] = adjustIslandForScreenSize();
 
   return (
     <section className="w-full h-screen relative">
@@ -32,13 +33,15 @@ const Home = () => {
         camera={{ near: 0.1, far: 1000}}
       >
         <Suspense fallback={<Loader />}>
-          <directionalLight />
-          <ambientLight/>
-          <pointLight />
-          <spotLight />
-          <hemisphereLight />
+          <directionalLight position={[1, 1, 1]} intensity={2} />
+          <ambientLight intensity={0.5}/>
+          <hemisphereLight skyColor="#b1e1ff" groundColor="#000000" intensity={1} />
 
-          <Island/>
+          <Island
+            position={islandPosition}
+            scale={islandScale}
+            rotation={islandRotation}
+          />
         </Suspense>
         
       </Canvas>
