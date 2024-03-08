9579fc435bb2944678a924151adbec08088f6987
vbasoon <vbasoon@gmail.com>
2024-03-08 18:43:18 +0200

    3-d scene island Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,6 +9,21 @@ import Island from '../models/Island';
 </div> */}
 
 const Home = () => {
+  const adjustIslandForScreenSize = () => {
+    let screenScale = null; 
+    let screenPosition = [0, -6.5, -43];
+
+    if(window.innerWidth < 768) {
+      screenScale = [0.9, 0.9, 0.9];
+    } else {
+      screenScale = [1, 1, 1];
+    }
+
+    return [screenScale, screenPosition]
+  }
+
+  const [islandScale, islandPosition] = adjustIslandForScreenSize();
+
   return (
     <section className="w-full h-screen relative">
       Home
