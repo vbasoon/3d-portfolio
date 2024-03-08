d239bcecd09a1ba65a8c92a8e44c2b27593a6500
vbasoon <vbasoon@gmail.com>
2024-03-08 18:28:05 +0200

    add 3-d scene Island

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,8 @@ import { Suspense } from 'react'
 import {Canvas} from '@react-three/fiber'
 import Loader from '../components/Loader'
 
+import Island from '../models/Island';
+
 {/* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
   POPUP
 </div> */}
@@ -15,7 +17,13 @@ const Home = () => {
         camera={{ near: 0.1, far: 1000}}
       >
         <Suspense fallback={<Loader />}>
+          <directionalLight />
+          <ambientLight/>
+          <pointLight />
+          <spotLight />
+          <hemisphereLight />
 
+          <Island/>
         </Suspense>
         
       </Canvas>
