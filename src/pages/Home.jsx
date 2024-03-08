cc1bdd9791cd2238cf1d139a3a076bde6e5d6726
vbasoon <vbasoon@gmail.com>
2024-03-08 19:14:45 +0200

    disapear model

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -37,6 +37,7 @@ const Home = () => {
           <ambientLight intensity={0.5}/>
           <hemisphereLight skyColor="#b1e1ff" groundColor="#000000" intensity={1} />
 
+          <Sky/>
           <Island
             position={islandPosition}
             scale={islandScale}
