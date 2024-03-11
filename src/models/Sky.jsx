5ac68bdfe9615d71c4532c952cb7b8a52e48d588
vbasoon <vbasoon@gmail.com>
2024-03-11 09:24:48 +0200

    Island before

--- src/models/Sky.jsx
+++ src/models/Sky.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 
+import { useGLTF } from '@react-three/drei'
+
+import skyScene from '../assets/3d/sky.glb'
+
 const Sky = () => {
+  const sky = useGLTF(skyScene);
+
   return (
-    <div>Sky</div>
+    <mesh>
+      <primitive object={sky.scene} />
+    </mesh>
   )
 }
 
