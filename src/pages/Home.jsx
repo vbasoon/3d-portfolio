30fa43894615e2e584e2ee160e807ac163b95ef2
vbasoon <vbasoon@gmail.com>
2024-03-12 11:18:19 +0200

    Home useStage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,6 +14,7 @@ import  Plane from '../models/Plane';
 const Home = () => {
 
   const [isRotating, setIsRotating] = useState(false); //State
+  const [currentStage, setCurrentStage] = useState(1)
 
   const adjustIslandForScreenSize = () => {
     let screenScale = null; 
