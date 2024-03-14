6869d4d6d555295969074bfad18623936349567f
vbasoon <vbasoon@gmail.com>
2024-03-14 10:59:42 +0200

    contact form

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -5,6 +5,19 @@ const Contacts = () => {
    <section className="relative flex lg:flex-row flex-col max-container">
     <div className='flex-1 min-w-[50%] flex flex-col'>
       <h1 className='head-text'>Get in Touch</h1>
+      <form className='w-full flex flex-col gap-7 mt-14'>
+        <label className='text-black-500 font-semibold'>
+          Name
+        </label>
+        <input 
+          type="text" 
+          name="name" 
+          className="input"
+          placeholder='Enter your name'
+          required
+          //value={} 
+        />
+      </form>
     </div>
    </section>
   )
