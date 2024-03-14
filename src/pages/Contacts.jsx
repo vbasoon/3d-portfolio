b4f97652be70e1704e32aae0273b847a7f3a391b
vbasoon <vbasoon@gmail.com>
2024-03-14 10:44:22 +0200

    Contacts

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,6 +1,12 @@
+import React from 'react'
+
 const Contacts = () => {
   return (
-    <div>Contacts</div>
+   <section className="relative flex lg:flex-row flex-col max-container">
+    <div className='flex-1 min-w-[50%] flex flex-col'>
+      <h1 className='head-text'>Get in Touch</h1>
+    </div>
+   </section>
   )
 }
 
