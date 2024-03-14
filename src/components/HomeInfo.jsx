dd3af42868142bac72264b2f10c910226f7ba4a1
vbasoon <vbasoon@gmail.com>
2024-03-14 08:12:00 +0200

    web-develop

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const renderContent = {
   1: (
-    <h1 className="sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5">Hi, My name is <span className='font-semibold'>Vlad</span></h1>
+    <h1 className="sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5">Hi, My name is <span className='font-semibold'>Vlad</span>
+    <br />
+    A Web-developer from Ukraine
+    </h1>
   ),
   2: (
     <h1>2</h1>
