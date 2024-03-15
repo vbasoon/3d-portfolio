bf66ba288858f9e55ab8a1e4cd99ee1b02a7e804
vbasoon <vbasoon@gmail.com>
2024-03-15 08:20:02 +0200

    Fox animation

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -8,7 +8,8 @@ import Loader from '../components/Loader.jsx'
 const Contacts = () => {
   const formRef = useRef(null);
   const [form, setForm] = useState({name: '', email: '', message: ''})
-  const [isLoading, setIsLoading] = useState(false)
+  const [isLoading, setIsLoading] = useState(false);
+  const [currentAnimation, setCurrentAnimation] = useState('idle');
 
   const handleChange = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value })
@@ -16,6 +17,7 @@ const Contacts = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     setIsLoading(true);
+    setCurrentAnimation('hit');
 
     emailjs.send(
       import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
@@ -33,15 +35,20 @@ const Contacts = () => {
       // TODO: Show success message
       // TODO: Hide an alert
 
-      setForm({name: '', email: '', message: ''})
+      setTimeout(() => {
+        setCurrentAnimation('idle')
+        setForm({name: '', email: '', message: ''});
+      }, [3000])
+
+      
     }).catch((error) => {
       setIsLoading(false);
       console.log(error);
       // TODO: Show error message
     })
   }
-  const handleFocus = () => {}
-  const handleBlur = () => {}
+  const handleFocus = () => setCurrentAnimation('walk');
+  const handleBlur = () => setCurrentAnimation('idle');
 
   return (
    <section className="relative flex lg:flex-row flex-col max-container">
@@ -115,10 +122,13 @@ const Contacts = () => {
           far: 1000
         }}
       >
+        <directionalLight intensity={2.5} position={[0, 0, 1]}/>
+        <ambientLight intensity={0.5}/>
         <Suspense fallback={<Loader/>}>
           <Fox
+            currentAnimation={currentAnimation}
             position={[0.5, 0.35, 0]}
-            rotation={[12, 0, 0]}
+            rotation={[12.6, -0.6, 0]}
             scale={[0.5, 0.5, 0.5]}
           />
         </Suspense>
