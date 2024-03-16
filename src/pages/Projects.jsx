22546fa1250fe90680b291db658355dcf1e91c12
vbasoon <vbasoon@gmail.com>
2024-03-16 09:23:46 +0200

    projects

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,6 +1,15 @@
+import React from 'react'
+
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <section className="max-container">
+      <h1 className="head-text">
+        Hello, I'm <span className='blue-gradient_text font-semibold drop-shadow'>Vlad</span>
+      </h1>
+      <div className="mt-5 flex flex-col gap-3 text-slate-500">
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic ratione vero est, delectus rerum doloribus nostrum suscipit fugiat commodi iusto?</p>
+      </div>
+    </section>
   )
 }
 
